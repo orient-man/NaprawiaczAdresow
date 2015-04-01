0deb54783846bf98f3873d67305bb934478349ec
orientman <orientman@gmail.com>
2015-04-01 12:38:38 +0200

    Obsługa Nullable<int>

--- NaprawiaczAdresow.fsx
+++ NaprawiaczAdresow.fsx
@@ -2,7 +2,7 @@
 open FSharp.Data
 
 [<Literal>]
-let jsonSampleInput = """{
+let jsonSampleInput = """[{
     "AdresID":186875,
     "Lokalizacja":{
         "Adres":"Wrocławska 25, 56-400 Oleśnica",
@@ -12,29 +12,53 @@ let jsonSampleInput = """{
         "NrDzialki":"tekst",
         "NrMieszkania":"tekst",
         "Poczta":{"key":21186,"value":"56-400"},
-        "Skrytka":"tekst",
+        "Skrytka":15,
         "Ulica":{"key":34645,"value":"Wrocławska"},
         "UwagiDodatkowe":"tekst"}
-}"""
+}, {
+    "AdresID":186875,
+    "Lokalizacja":{
+        "Adres":"Wrocławska 25, 56-400 Oleśnica",
+        "ID":84915,
+        "Miejscowosc":{"key":30265,"value":"Oleśnica"},
+        "NrDomu":"teskt",
+        "NrDzialki":"tekst",
+        "NrMieszkania":"tekst",
+        "Poczta":{"key":21186,"value":"56-400"},
+        "Skrytka":null,
+        "Ulica":{"key":null,"value":"Wrocławska"},
+        "UwagiDodatkowe":"tekst"}
+}]"""
 
 [<Literal>]
-let jsonSampleOutput = """{
+let jsonSampleOutput = """[{
     "Adres":"Wrocławska 25, 56-400 Oleśnica",
     "Miejscowosc":{"key":30265,"value":"Oleśnica"},
     "NrDomu":"napis",
     "NrDzialki":"tekst",
     "NrMieszkania":"tekst",
     "Poczta":{"key":21186,"value":"56-400"},
-    "Skrytka":"tekst",
+    "Skrytka":15,
     "Ulica":{"key":34645,"value":"Wrocławska"},
     "UwagiDodatkowe":"tekst"
-}"""
+}, {
+    "Adres":"Wrocławska 25, 56-400 Oleśnica",
+    "Miejscowosc":{"key":30265,"value":"Oleśnica"},
+    "NrDomu":"napis",
+    "NrDzialki":"tekst",
+    "NrMieszkania":"tekst",
+    "Poczta":{"key":21186,"value":"56-400"},
+    "Skrytka":null,
+    "Ulica":{"key":null,"value":"Wrocławska"},
+    "UwagiDodatkowe":"tekst"
+}]"""
 
-type JsonInput = JsonProvider<jsonSampleInput>
+type JsonInput = JsonProvider<jsonSampleInput, SampleIsList=true>
 
-type JsonOutput = JsonProvider<jsonSampleOutput, RootName="Lokalizacja">
+type JsonOutput = JsonProvider<jsonSampleOutput, SampleIsList=true, RootName="Lokalizacja">
 
 type KVP = JsonOutput.Miejscowosc
+type NKVP = JsonOutput.Ulica
 
 let napraw input = 
     input
@@ -45,5 +69,5 @@ let napraw input =
             (adres = lok.Adres, miejscowosc = KVP(lok.Miejscowosc.JsonValue), nrDomu = lok.NrDomu, 
              nrDzialki = lok.NrDzialki, nrMieszkania = lok.NrMieszkania, 
              poczta = KVP(lok.Poczta.JsonValue), skrytka = lok.Skrytka, 
-             ulica = KVP(lok.Ulica.JsonValue), uwagiDodatkowe = lok.UwagiDodatkowe)
+             ulica = NKVP(lok.Ulica.JsonValue), uwagiDodatkowe = lok.UwagiDodatkowe)
     |> fun output -> output.JsonValue.ToString()
